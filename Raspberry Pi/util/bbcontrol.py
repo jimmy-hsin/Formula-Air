39bf04a2d8aa32c2784a638302be7f8c119287b8
jimmy-hsin <r09945014@ntu.edu.tw>
2025-01-13 22:09:55 +0800

    Add files via upload

---
+++ Raspberry Pi/util/bbcontrol.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+'''
+Class definition for control system
+
+'''
+import numpy as np
+import sys
+import os
+import time
+
+sys.path.append(os.path.abspath('..'))
+from config import config
+
+class control:
+
+	def __init__(self):
+		print('Controller initialized!')
+
+	def maplocation(self,pts):
+		self.lowindex = config.C1
+		self.uppindex = config.C2 + self.lowindex
+		lower = np.mean(pts[0:self.lowindex])
+		upper = np.mean(pts[self.lowindex:self.uppindex-1])
+		print("lower")
+		print(pts[0:self.lowindex])
+
+		lowvect = np.diff(pts[0:self.lowindex])
+		lowvectmean  = np.mean(lowvect)
+		print("lowvec")
+		print(lowvect)
+		#print(pts[self.lowindex:self.uppindex-1])
+		#print(lower)
+		#print(upper)
+		#print(upper-lower)
+		if lower < 100 and abs(upper-lower) > 150:
+			return False
+		else:
+			return False
+
+	def detlevel(self, pts, mapcond):
+		lev = 0
+		if mapcond == 0:
+			npts = self.outlier_reject(pts[1:])
+			npts = pts[1:]
+			devi = np.mean(npts) - 340
+			if devi < 50 and devi > -40:
+				lev = 0
+			elif devi < -40:
+				lev = -1
+			else:
+				lev = 1
+
+		elif mapcond == 1:
+			lev = 2
+			mapcond = 1
+			print("Find Sharp turn")
+
+		return lev
+
+	def outlier_reject(self,pts,tol = 1):
+		cpts = np.copy(pts)
+		s = np.std(pts)
+		m = np.mean(pts)
+		z = (pts-m)/s
+		index = np.where(abs(z)>1)
+		cpts = np.delete(cpts,index)
+		return cpts
+
---
+++ Raspberry Pi/util/findline2.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+# This is the image processing unit for finding
+# black line to trace in the given image.
+
+import numpy as np
+import cv2
+import time
+import sys
+import os
+
+sys.path.append(os.path.abspath('..'))
+from config import config
+
+class findline:
+	# class memebers:
+	kernel = np.ones((7,7), np.uint8)
+
+	# Object constructor:
+	def __init__(self,img):
+		self._img = np.copy(img)
+		self._raw = np.copy(img)
+		self.RES = config.RES
+		self.num = config.NUMofCUT
+		self.xs =  config.CUTHEIGHT
+		# print('cut:',self.xs)
+		self.tol = config.tol
+		self.CENTER = config.CENTER
+		self.sharpturn = 0
+
+	def detectline(self, stdev=300):
+		t0 = time.perf_counter()
+		gimg = cv2.cvtColor(self._img,cv2.COLOR_BGR2GRAY)
+		cimg = cv2.GaussianBlur(gimg, (5, 5), 0)
+		ret,bimg = cv2.threshold(cimg,90,255,cv2.THRESH_BINARY)
+		oimg = cv2.morphologyEx(bimg, cv2.MORPH_OPEN, self.kernel)
+		oimg = cv2.morphologyEx(oimg, cv2.MORPH_CLOSE,self.kernel)
+		self.midpoints = []
+		self.midpoints.append(stdev)
+
+		for i in range(0,self.num):
+			r = len(self.midpoints)-1
+			self.needcheck = False
+			h = self.xs[i]
+			cut = oimg[h].astype(np.int16)
+			df = np.diff(cut)
+			points = np.where(np.logical_or(df > 200, df < -200))
+			print('iteration = ',i)
+			print('CUTHEIGHT: =', h)
+			print('len of points[0]: ',len(points[0]))
+
+			if len(points) > 0 and len(points[0]) == 2:
+				if i == 0:
+					Lpt = points[0][0]
+					Rpt = points[0][1]
+					midpoint = int((points[0][0] + points[0][1]) / 2)
+					print('Safe')
+				else:
+					if abs(self.midpoints[r]-(points[0][0] + points[0][1])/2) <= self.tol:
+						Lpt = points[0][0]
+						Rpt = points[0][1]
+						midpoint = int((points[0][0] + points[0][1]) / 2)
+						print('Safe')
+			elif len(points[0]) == 1 and points[0][0] < 50:
+				print("Detect potential sharp turn??")
+				self.sharpturn = 1
+				midpoint = 0
+
+			else:
+				self.needcheck = True
+				print('Need to Confirm')
+
+			if self.needcheck:
+				cut = cimg[h].astype(np.int16)
+				cut[cut > 160] = 160
+				df = np.diff(cut)
+				Lpt = np.where(df < -8)
+				Rpt = np.where(df > 8)
+				if len(Lpt[0]) < 1 or len(Rpt[0]) <1:
+					midpoint = np.argmin(cut)
+				else:
+					RR = len(Rpt[0])-1
+					midpoint = int((Lpt[0][0]+Rpt[0][RR])/2)
+				'''
+
+				if abs(self.midpoints[r]-midpoint) > self.tol:
+					print('Cannot find the black line!')
+					x = input('Save? 1 = Yes.')
+					if x == '1':
+						naming = 'debugger' + str(i) + '.bmp'
+						namer = 'rawimage' + str(i) +'.bmp'
+						cv2.imwrite(naming,self._img)
+						cv2.imwrite(namer,self._raw)
+						print('Debug image saved')
+					continue
+				'''
+
+			self.midpoints.append(midpoint)
+			'''
+			cv2.circle(self._img, (midpoint, h), 8, (0,255,0), -1)
+			cv2.circle(self._img, (self.CENTER, h), 8, (255,0,0), -1)
+
+			if not self.needcheck:
+				cv2.circle(self._img, (Lpt, h), 5, (0,0,255), -1)	
+				cv2.circle(self._img, (Rpt, h), 5, (0,0,255), -1)
+				cv2.rectangle(self._img, (Lpt-15, h-15),(Rpt+15, h+15) , (228, 255, 109), 2)
+			else:
+				cv2.rectangle(self._img, (midpoint-20, h-15),(midpoint+20, h+15) , (228, 255, 109), 2)
+
+			cv2.imshow('viewer',self._img)
+			cv2.waitKey(0)
+			'''
+
+		print (time.perf_counter()-t0)
+		return self.midpoints, self._img, self.sharpturn
+
+	def markline(self,pts):
+		for i in range(1,self.num):
+			h = self.xs[i-1]
+			if i>1 and self.xs[i-2] == h:
+				continue
+			midpoint = pts[i]
+			cv2.circle(self._img, (midpoint, h), 8, (0,255,0), -1)
+
+		#cv2.imshow('viewer',self._img)
+		#cv2.waitKey(0)
+		return self._img
+		'''
+		x = input('filename:')
+		if x == '0':
+			return 
+		fname = '/Users/wayne/Desktop/'+x+'.bmp'
+		cv2.imwrite(fname,self._img)
+		'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
